fc0b6e1cf294b873394e81112d027874f05f6f01
Tijs Nieuwboer <Tijs@lerai.nl>
2025-11-29 16:27:01 +0100

    Allow all HTTPS image domains

--- next.config.ts
+++ next.config.ts
@@ -5,27 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.b-cdn.net",
-      },
-      {
-        protocol: "https",
-        hostname: "ethglobal.b-cdn.net",
+        hostname: "**",
       },
     ],
   },
