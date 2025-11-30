c31a0a2100ece5afd1f81547f5ec3312a8e9f85f
Jordan <39-12602@sae.edu.za>
2025-11-30 13:56:04 +0100

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "League - Builder Platform",
+    default: "MEGAS - Builders Unite",
     template: "%s | League",
   },
   description:
